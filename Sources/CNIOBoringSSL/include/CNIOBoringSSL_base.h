f0de289ffaa0e605df9193428b0865603c0b261f
Pascal <me@pascal.sh>
2026-09-08 19:24:42 -0700

    Disable BoringSSL assembly on x86 Windows where SwiftPM cannot assemble NASM

--- Sources/CNIOBoringSSL/include/CNIOBoringSSL_base.h
+++ Sources/CNIOBoringSSL/include/CNIOBoringSSL_base.h
@@ -12,6 +12,9 @@
 
 #define BORINGSSL_PREFIX CNIOBoringSSL
 
+#if defined(_WIN32) && (defined(__x86_64) || defined(_M_AMD64) || defined(_M_X64) || defined(__x86) || defined(__i386) || defined(__i386__) || defined(_M_IX86))
+#define OPENSSL_NO_ASM
+#endif
 
 // This file should be the first included by all BoringSSL headers.
 
